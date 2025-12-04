315016286440a40a195e8416a9e8ab8847badf46
Md Wahiduzzaman Khan Siyam <wksiyam@WKs-MacBook-Air.local>
2025-12-04 12:59:15 +0600

    fifth commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,19 +2,26 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
+
 class UserRegistrationForm(UserCreationForm):
     """User registration form"""
-    
+
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
-    
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'role', 'password1', 'password2']
-    
+        fields = ['username', 'email', 'first_name',
+                  'last_name', 'role', 'password1', 'password2']
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Limit role choices to exclude ADMIN
+        self.fields['role'].choices = [
+            (User.Role.INSTRUCTOR, 'Instructor'),
+            (User.Role.STUDENT, 'Student'),
+        ]
         # Add Bootstrap classes
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control'})
@@ -22,13 +29,13 @@ class UserRegistrationForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
     """User update form"""
-    
+
     email = forms.EmailField()
-    
+
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
@@ -37,7 +44,7 @@ class UserUpdateForm(forms.ModelForm):
 
 class ProfileUpdateForm(forms.ModelForm):
     """Profile update form"""
-    
+
     class Meta:
         model = User
         fields = ['bio', 'profile_picture', 'phone', 'date_of_birth']
@@ -45,9 +52,10 @@ class ProfileUpdateForm(forms.ModelForm):
             'bio': forms.Textarea(attrs={'rows': 4}),
             'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
         }
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             if field != 'profile_picture':
-                self.fields[field].widget.attrs.update({'class': 'form-control'})
+                self.fields[field].widget.attrs.update(
+                    {'class': 'form-control'})
